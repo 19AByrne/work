07d433337848e153b137573acdc0fc96f4d51eb8
19AByrne <144443545+19AByrne@users.noreply.github.com>
2023-09-20 19:18:54 +0100

    Add files via upload

--- list exercise 78.py
+++ list exercise 78.py
@@ -1,7 +1,10 @@
 #list exercise 78
-tv = ["Atlanta", "Better Call Saul", "Modern Family", "It's always sunny in philadelphia"]
+tv = ["Atlanta", "Better Call Saul", "Modern Family", "It's Always Sunny in Philadelphia"]
 print(tv[0])
 print(tv[1])
 print(tv[2])
 print(tv[3])
-#unfinished work
+usershow = str(input("Enter another show. "))
+tv.append(usershow)
+tv.sort()
+print(*tv, sep=", ")
