cc5a503e705e5f9de202c2a6375a4bbccfc83e6a
19AByrne <144443545+19AByrne@users.noreply.github.com>
2023-10-04 13:14:37 +0100

    Add files via upload

---
+++ caesar cipher.py
@@ -0,0 +1,26 @@
+#caesar cipher
+"""
+part 1
+alphabaT = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+alphabet = ['D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C']
+part 2
+DWKOQH FRPPXQLWB FROOHDH
+part 3
+
+part 4
+WHAT DO YOU GET WHEN YOU CROSS A SNOWMAN WITH A VAMPIRE? FROSTBITE
+"""
+
+Alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C']
+inp = str(input("Enter any phrase.\n"))
+inp = inp.lower()
+count = 0
+for x in inp:
+    if inp[count] in Alphabet:
+        inp = inp.replace(inp[count], alphabet[Alphabet.index(inp[count])])
+    else:
+        pass
+    count +=1
+print(inp)
+    
