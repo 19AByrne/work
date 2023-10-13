786c2cd129e702f0201371e4e6777cbc4ae147ee
19AByrne <144443545+19AByrne@users.noreply.github.com>
2023-10-13 12:34:42 +0100

    Add files via upload

--- turtle exercise 63.py
+++ turtle exercise 63.py
@@ -3,10 +3,11 @@ import turtle
 t = turtle
 import random
 r = random
-colours = ['red', 'black', 'purple']
-def square():
-    t.color = ('black', r.choice(colours))
-    for x in range(4):
-        t.forward(100)
-        t.left(90)
-square()
+t.fillcolor = ('red')
+t.begin_fill()
+for x in range(4):
+    t.forward(100)
+    t.left(90)
+t.end_fill()
+t.fillcolor = ('red')
+
