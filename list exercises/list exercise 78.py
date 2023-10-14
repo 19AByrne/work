c8fcc31fb254f332d08473b7109dc12f12e6c416
19AByrne <144443545+19AByrne@users.noreply.github.com>
2023-10-14 01:45:53 +0100

    Update list exercise 78.py

--- list exercises/list exercise 78.py
+++ list exercises/list exercise 78.py
@@ -1,7 +1,7 @@
 #list exercise 78
 tv = ["Atlanta", "Better Call Saul", "Modern Family", "It's always sunny in philadelphia"]
-print(tv[0])
-print(tv[1])
-print(tv[2])
-print(tv[3])
-#unfinished work
+print(*tv, sep="\n")
+show = str(input("Enter another show\n"))
+pos = int(input("What position do you want it in out of 5?\n"))
+tv.insert(pos, show)
+print(*tv, sep="\n")
