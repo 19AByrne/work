51cec7ca74d2811c5aaaf5c7d8e09ad7c82e75e9
19AByrne <144443545+19AByrne@users.noreply.github.com>
2023-10-22 23:15:07 +0100

    Add files via upload

--- solo work/the chaos game.py
+++ solo work/the chaos game.py
@@ -3,6 +3,8 @@ import random
 import time
 t = turtle
 t.speed(10)
+t.Screen().bgcolor('black')
+t.color('white')
 def point_on_triangle(pt1, pt2, pt3):
     """
     Random point on the triangle with vertices pt1, pt2 and pt3.
@@ -11,23 +13,15 @@ def point_on_triangle(pt1, pt2, pt3):
     s, t, u = x, y - x, 1 - y
     return (s * pt1[0] + t * pt2[0] + u * pt3[0],
             s * pt1[1] + t * pt2[1] + u * pt3[1])
-
 t.penup()
-# t.hideturtle()
-# for x in range(3):
-#     t.forward(200)
-#     t.left(120)
-
 
 p1 = t.pos()
-t.forward(300)
+t.forward(400)
 p2 = t.pos()
 t.left(120)
-t.forward(300)
+t.forward(400)
 p3 = t.pos()
 
-
-t.penup()
 points = [p1,p2,p3]
 ranpos = point_on_triangle(p1,p2,p3)
 while 1==1:
