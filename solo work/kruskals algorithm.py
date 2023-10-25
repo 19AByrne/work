3678a2cf22d60df1581e70a25f6a7201234aab71
19AByrne <144443545+19AByrne@users.noreply.github.com>
2023-10-25 13:11:03 +0100

    Update kruskals algorithm.py

--- solo work/kruskals algorithm.py
+++ solo work/kruskals algorithm.py
@@ -30,12 +30,13 @@ for x in range(nodes):
             pass
         else:
             inpadj = int(input(f"What is the adjacency of edge {selectededge}\n"))
-            adj.update({selectededge:inpadj})
-            adjkey.append(selectededge)
+            if inpadj >= 1:
+                adj.update({selectededge:inpadj})
+                adjkey.append(selectededge)
+            else:
+                pass
     done.append(alpha[x])
-#     print(done)
-# print(adj)
-# print(adjkey)
+
 for x in range(len(adjkey)):
     selectededge = adjkey[x]
     if adj[selectededge] == 1:
@@ -43,28 +44,22 @@ for x in range(len(adjkey)):
         t.goto(ps[selectededge[0]])
         t.pendown()
         t.goto(ps[selectededge[1]])
-##########-------------  
-##error here fix it         
-# done = []
-# weight = {}
-# weightkey = []
-# for x in range(nodes):
-# #     print(f"{x} x")
-#     for i in range(nodes):
-# #         print(f"{i} i")
-#         selectededge = (pskey[x]+pskey[i])
-#         if selectededge[0] in done or selectededge[1] in done:
-#             pass
-#         else:
-#             inpweight = int(input(f"What is the weight of edge {selectededge}\n"))
-#             weight.update({selectededge:inpweight})
-#             weightkey.append(selectededge)
-#     done.append(alpha[x])
-# print(weight)
-# print(weightkey)
-##########------------- 
 
-# print(ps)
-# print(pskey)
+
+print(adj)
+print(adjkey)
+      
+done = []
+weight = {}
+weightkey = []
+for x in range(len(adj)):
+    selectededge = adjkey[x]
+    inpweight = int(input(f"What is the weight of edge {selectededge}\n"))
+    weight.update({selectededge:inpweight})
+    weightkey.append(selectededge)
+    done.append(alpha[x])
+print(weight)
+print(weightkey)
+
 
 t.done()
