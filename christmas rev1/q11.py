4ab4eb121a1a375b9c97ef5ed8025a959df4e373
19AByrne <144443545+19AByrne@users.noreply.github.com>
2023-11-28 23:59:37 +0000

    Add files via upload

---
+++ christmas rev1/q11.py
@@ -0,0 +1,6 @@
+#christmas revision exercise 1 q11 #i still dont know what this is cant test it#
+principle = int(input("enter principle: "))
+rate = int(input("enter rate: "))
+time = int(input("enter time: "))
+ci = principle*(1+rate/100)**time-principle
+print(ci)
