c95a27f3c65f5258c2bf341ecacd1bcdffa4dd6e
19AByrne <19AByrne.ACC@lwetb.ie>
2024-04-12 12:27:56 +0100

    awdsa

---
+++ Flowchart/smallest number.py
@@ -0,0 +1,6 @@
+NUM1 = int(input("Enter number 1: "))
+NUM2 = int(input("Enter number 2: "))
+if NUM1 < NUM2:
+    print(f"{num1} is the smallest number")
+else:
+    print(f"{num2} is the smallest number")
