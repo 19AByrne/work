5c8c0afb46c4a7eb5dcd2b3ede68f3ed890fd3b3
19AByrne <144443545+19AByrne@users.noreply.github.com>
2024-09-02 19:25:55 +0100

    Update simple_selection_sort.py

--- sorting algo/simple_selection_sort.py
+++ sorting algo/simple_selection_sort.py
@@ -1,10 +1,11 @@
 def simple_selection(L):
+    
     for m in range(len(L)):
+        smalli = m
         for i in range(m+1,len(L)):
-            if L[i] < L[m]:
-                sml = i
-        
-    print(L[sml])
-    return L,i
+            if L[smalli] > L[i]:
+                smalli = i
+        L[m], L[smalli] = L[smalli], L[m]
+    return L
 
 print(simple_selection([9,2,3,7,5]))
