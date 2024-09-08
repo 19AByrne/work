3b2b40c91fe9a20b0ad1130299c7ae6372cf9fc1
ForeZone <aaronjamesbyrne7@gmail.com>
2024-09-08 13:05:14 +0100

    Update averages.py

--- averages.py
+++ averages.py
@@ -17,13 +17,32 @@ def Mode(L):
     for i in range(len(L)):
         if NL[i] > NL[big]:
             big = i
-            
+    print(f'big = {big},')
+    print(L)
     return L[big]
 
+def antimode(L):
+    NL=[]
+    for x in range(len(L)):
+        NL.append(0)
+    for x in range(len(L)):
+        xcount = 0
+        for y in L:
+            if y == L[x]:
+                xcount+=1
+            else:
+                pass
+        NL[x] = xcount
+        
+    small = 0
+    for i in range(len(L)):
+        if NL[i] < NL[big]:
+            small = i
+    return L[small]
+
 def Median(L):
-        return L[round(len(L)/2)] # cheeeeck
-    
-    
+        return L[round(len(L)/2)+1] 
+
 def Frequency(L,t):
     count = 0
     for x in L:
@@ -32,10 +51,23 @@ def Frequency(L,t):
     return count
 
 def Top5(L):
-    pass
+    top5 = []
+    NL = L.copy()
+    for x in range(5):
+        print('pass')
+        m = Mode(NL)
+        top5.append(m)
+        print(NL)
+        for i in range(NL.count(m)):
+            NL.remove(m)
+    
+    print(top5)
+    print('done') 
     
 def Last5(L):
-    pass
+    last5 = []
+    NL = L.copy()
+    for x in range
     
 while 1:
     print('Enter the length of your list')
@@ -43,11 +75,14 @@ while 1:
     print('Enter your sorted list 1 by 1')
     user_list = [int(input()) for x in range(length)]
     while 1:
-    print('''1) Mean
+        print('''1) Mean
 2) Median
 3) Mode
 4) Frequency
 5) Top 5
 6) Last 5''')
-    if int(input()) == 1:
-        Mean(user_list)
+        input()
+        print(Last5([3, 4, 8, 9, 9, 10, 10, 11, 11, 11, 12, 12, 12, 12]))
+
+
+
