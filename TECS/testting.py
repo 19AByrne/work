c397509e2cd633c39b31b6486b5ae9078bd5d384
19AByrne <144443545+19AByrne@users.noreply.github.com>
2024-10-25 22:57:27 +0100

    Update testting.py

--- TECS/testting.py
+++ TECS/testting.py
@@ -13,8 +13,10 @@ import math
 
 '''commercial use''' #pygame.display.get_desktop_sizes <<<<use that like
 pygame.init()
-height = 1080
-width = 1920
+wh = pygame.display.get_desktop_sizes()[0]
+print(wh)
+height = wh[1]
+width = wh[0]
 screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 running = True
@@ -22,8 +24,10 @@ running = True
 scale = width/50
 g = 9.8
 
-initial = (6,24)
+initial = (3,10)
 
+xshift = 0
+yshift = 0
 
 theta = math.degrees(math.atan(initial[1]/initial[0]))
 mag = math.sqrt((initial[0])**2+(initial[1])**2)
@@ -40,8 +44,8 @@ def xrange(init):
 def maxheight(init):
     return ( (mag**2) * (math.sin(math.radians(theta)))*(math.sin(math.radians(theta))) ) / (2*g)
 
-print(xrange(initial))
-print(maxheight(initial))
+# print(xrange(initial))
+# print(maxheight(initial))
 
 
 while running:
@@ -50,19 +54,31 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 running = False
+            if event.key == pygame.K_UP:
+                yshift -= 10
+            if event.key == pygame.K_DOWN:
+                if yshift != 0:
+                    yshift += 10
+            if event.key == pygame.K_RIGHT:
+                xshift += 10
+            if event.key == pygame.K_LEFT:
+                if xshift != 0:
+                    xshift -=10
+            if event.key == pygame.K_r:
+                xshift, yshift = 0,0
     screen.fill("black")
     
-    ground = pygame.Rect(0,height*7/8,width,height/8)
+    ground = pygame.Rect(0 , (height*7/8) - yshift ,width,height/8)
     pygame.draw.rect(screen, 'dark green', ground)
     
-    pygame.draw.circle(screen, 'red', (width/8,height*7/8), 5)
-    pygame.draw.circle(screen, 'red', (width/8 + (xrange(initial)*scale), height*7/8), 5)
+    pygame.draw.circle(screen, 'red', (width/8 -xshift,height*7/8 - yshift), 5)
+    pygame.draw.circle(screen, 'red', (width/8 + (xrange(initial)*scale) - xshift, height*7/8 - yshift), 5)
     maxpoint = (width/8 + (xrange(initial)*scale)/2 , (height*7/8) + (maxheight(initial)*scale))
-    print(maxpoint)
+#     print(maxpoint)
 
-    pygame.draw.circle(screen, 'purple', (width/8 + (xrange(initial)*scale)/2 , (height*7/8) - (maxheight(initial)*scale)), 5)
+    pygame.draw.circle(screen, 'purple', (width/8 + (xrange(initial)*scale)/2 -xshift,  (height*7/8) - (maxheight(initial)*scale)-yshift), 5)
     
     for x in range(43):
-        pygame.draw.circle(screen, 'blue', (width/8 + x*(scale), height*7/8), 3)
+        pygame.draw.circle(screen, 'blue', (width/8 + x*(scale)-xshift, (height*7/8) - yshift), 3)
     pygame.display.flip()
     clock.tick(144)  # fps limit
