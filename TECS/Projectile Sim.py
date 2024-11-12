1d26403b669527d795c2bb75efd49bf7c93d6958
ForeZone <aaronjamesbyrne7@gmail.com>
2024-11-12 00:25:03 +0000

    tecs

--- TECS/Projectile Sim.py
+++ TECS/Projectile Sim.py
@@ -27,7 +27,7 @@ running = True
 scale = width/50
 g = 9.8
 
-initial = (10,10)
+initial = (4,15)
 
 xshift = 0
 yshift = 0
@@ -37,7 +37,7 @@ mag = math.sqrt((initial[0])**2+(initial[1])**2)
 
 
 def time(init):
-    return (-(init[1])) / (-4.9)
+    return ((init[1])) / (4.9)
 
 # def xrange2(init):                  not using this function because it relies on another function
 #     return (init[0]) * (time(init))
@@ -48,37 +48,24 @@ def xrange(init):
 def maxheight(init):
     return ( (mag**2) * (math.sin(math.radians(theta)))*(math.sin(math.radians(theta))) ) / (2*g)
 
-# print(xrange(initial))
-# print(maxheight(initial))
-
-# print(time(initial))
-
-# def parabola(init, t):
-#     interval = 9999
-#     times = [(t*x)/interval for x in range(interval+1)]
-#     ypoints = [(initial[1]*time) - ((g/2)*(time**2)) for time in times]
-#     xpoints = [(initial[0]*time) for time in times]
-#     points = [ (xpoints[i],ypoints[i]) for i in range(len(xpoints)) ]
-#     return points
-
-def fire(init, t):
-    print(t)
-    time = round(t*1000) #need to round because timer cannot contain float
-    pygame.time.set_timer(landing, time, 1) #not sure what the first parameter means , 1 = loop parameter
-    times = [(t*x)/time for x in range(time)]
-    #if i dont use (time+1) the lengths of intervals and ms wll be equal but the final position(landing coord) will not be included, not sure if creates issues
-    for time in times:
-        pygame.draw.circle(screen, 'white', (width/8 + (init[0]*time)*scale -xshift, (height*7/8) - ((init[1]*time) - ((g/2)*(time**2)))*scale - yshift), 1)
-
 def calculate_pos(t, ux, uy, g, scale):
     x = (width/8 +(ux*t)*scale - xshift)
     y = (height*7/8 - (uy*t - (g/2)*(t**2))*scale - yshift)
     return (x,y)
     
+    
+    
+    
+    
+
+showtrail = False    
 landing = pygame.event.custom_type()
 per_ms = pygame.event.custom_type()
-
+simulating = False
+landed = False
 curvepoints = []
+totalT = 0
+
 while running:
     dT = clock.get_time()
     for event in pygame.event.get():
@@ -106,21 +93,45 @@ while running:
                 xshift, yshift = 0,0
                 scale = width/50
                 
-            if event.key == pygame.K_SPACE:
-                pygame.time.set_timer(landing, round(time(initial)*1000), 1)
                 
-                airtime = time(initial)
-                current_time = pygame.time.get_ticks() / 1000
-                if current_time < airtime:
-                    pos = calculate_pos(current_time, initial[0], initial[1], g, scale)
-                    curvepoints.append(pos)
+                
+                
+            ####keys to be turned into buttons
+            if event.key == pygame.K_SPACE:
+                if not simulating:
+                    pygame.time.set_timer(landing, round(time(initial)*1000), 1)
+                    simulating = True
+                    path = []
+                
+            if event.key == pygame.K_m:  
+                simulating = False
+                landed = False
+                curvepoints = []
+                totalT = 0
+                path = []
+            
+            if event.key == pygame.K_n:
+                showtrail = not showtrail
             
         
         if event.type == landing:
-            print(f'{time(initial)} elapsed')
+            simulating = False
+            landed = True
+            print(f'{time(initial)} elapsed') #not necessary
             
-                
     screen.fill("black")
+    
+    if simulating:
+        totalT += dT
+#         print(totalT/1000)
+        pos = calculate_pos(totalT/1000, initial[0], initial[1], g, scale)
+        path.append(pos)
+        if showtrail:
+            for p in path:
+                pygame.draw.circle(screen, 'white', p, 3)
+        else:
+            pygame.draw.circle(screen, 'white', pos, 3)
+
     
     ground = pygame.Rect(0 , (height*7/8) - yshift ,width,height/8)
     pygame.draw.rect(screen, 'dark green', ground) #ground
@@ -129,14 +140,12 @@ while running:
     pygame.draw.circle(screen, 'red', (width/8 + (xrange(initial)*scale) - xshift, height*7/8 - yshift), 5) #final pos
     maxpoint = (width/8 + (xrange(initial)*scale)/2 , (height*7/8) + (maxheight(initial)*scale)) #max height
 
-
     pygame.draw.circle(screen, 'purple', (width/8 + (xrange(initial)*scale)/2 -xshift,  (height*7/8) - (maxheight(initial)*scale)-yshift), 5)
     
     for x in range(100):
         pygame.draw.circle(screen, 'blue', (width/8 + x*(scale)-xshift, (height*7/8) - yshift), 3)
-        
-    for point in curvepoints:
-        pygame.draw.circle(screen, 'white', point, 3)
+    
 
     pygame.display.flip()
     clock.tick(60)  # fps limit
+
